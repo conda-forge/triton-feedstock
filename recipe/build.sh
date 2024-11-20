5ab4145a97deac29648ed3b1b59831d333c49cf5
Michał Górny <mgorny@gentoo.org>
2024-11-20 17:30:09 +0100

    Initial bump to 3.1.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,5 +5,10 @@ set -ex
 # remove outdated vendored headers
 rm -rf $SRC_DIR/python/triton/third_party
 
+export JSON_SYSPATH=$PREFIX
+# enable LLVM_SYSPATH when we can unvendor it (i.e. have a version match)
+# export LLVM_SYSPATH=$PREFIX
+export PYBIND11_SYSPATH=$SP_DIR/pybind11
+
 cd python
 $PYTHON -m pip install . -vv
