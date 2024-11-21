5b057953c44118cd0e8546ffc16e49c8d7887316
Michał Górny <mgorny@gentoo.org>
2024-11-21 16:12:49 +0100

    Unvendor nvidia dependencies

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,14 @@ export JSON_SYSPATH=$PREFIX
 export LLVM_SYSPATH=$PREFIX
 export PYBIND11_SYSPATH=$SP_DIR/pybind11
 
+# these don't seem to be actually used, but they prevent downloads
+export TRITON_PTXAS_PATH=$PREFIX/bin/ptxas
+export TRITON_CUOBJDUMP_PATH=$PREFIX/bin/cuobjdump
+export TRITON_NVDISASM_PATH=$PREFIX/bin/nvdisasm
+export TRITON_CUDACRT_PATH=$PREFIX
+export TRITON_CUDART_PATH=$PREFIX
+export TRITON_CUPTI_PATH=$PREFIX
+
 export MAX_JOBS=$CPU_COUNT
 
 cd python
