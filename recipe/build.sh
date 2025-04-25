b8e3bf77302abebb9c2731595e34e5e3bcb23a61
Michał Górny <mgorny@gentoo.org>
2025-04-25 20:22:50 +0200

    Fix aarch64 cross-builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -25,18 +25,55 @@ export MAX_JOBS=$CPU_COUNT
 # no easy way of passing this, not really worth a whole patch
 sed -i -e '/TRITON_BUILD_UT/s:\bON:OFF:' CMakeLists.txt
 
+CMAKE_HOST_ARGS=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DLLVM_BUILD_UTILS=OFF
+    -DLLVM_BUILD_TOOLS=OFF
+    -DLLD_BUILD_TOOLS=OFF
+    -DLLVM_BUILD_TELEMETRY=OFF
+    -DLLVM_ENABLE_PROJECTS="mlir;lld"
+    -DLLVM_TARGETS_TO_BUILD="host;NVPTX;AMDGPU"
+    -DLLVM_ENABLE_TERMINFO=OFF
+    -DLLVM_INCLUDE_TESTS=OFF
+    -DMLIR_INCLUDE_TESTS=OFF
+)
+
 # build LLVM first
-cmake -G Ninja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_BUILD_UTILS=OFF \
-    -DLLVM_BUILD_TOOLS=OFF \
-    -DLLD_BUILD_TOOLS=OFF \
-    -DLLVM_BUILD_TELEMETRY=OFF \
-    -DLLVM_ENABLE_PROJECTS="mlir;lld" \
-    -DLLVM_TARGETS_TO_BUILD="host;NVPTX;AMDGPU" \
-    -DLLVM_ENABLE_TERMINFO=OFF \
-    -Bllvm-project/build \
-    llvm-project/llvm
+if [[ ${HOST} != ${TARGET} ]]; then
+    CMAKE_BUILD_ARGS=(
+        -DCMAKE_C_COMPILER="${CC_FOR_BUILD}"
+        -DCMAKE_CXX_COMPILER="${CXX_FOR_BUILD}"
+        -DCMAKE_BUILD_TYPE=Release
+        -DLLVM_ENABLE_ZSTD=OFF
+        -DLLVM_ENABLE_LIBXML2=OFF
+        -DLLVM_ENABLE_ZLIB=OFF
+        -DLLVM_ENABLE_PROJECTS="mlir"
+    )
+    NATIVE_EXECUTABLES=(
+        llvm-tblgen
+        mlir-tblgen
+        mlir-linalg-ods-yaml-gen
+        mlir-src-sharder
+        mlir-pdll
+    )
+
+    cmake -G Ninja "${CMAKE_BUILD_ARGS[@]}" \
+        -Bllvm-project/build-native llvm-project/llvm
+    cmake --build llvm-project/build-native -j "${MAX_JOBS}" \
+        -t "${NATIVE_EXECUTABLES[@]}"
+
+    NATIVE_BIN=$PWD/llvm-project/build-native/bin
+    CMAKE_HOST_ARGS+=(
+        -DCMAKE_CROSSCOMPILING=ON
+        -DLLVM_NATIVE_TOOL_DIR=$PWD/llvm-project/build-native/bin
+        #-DLLVM_TABLEGEN=$NATIVE_BIN/llvm-tblgen
+        #-DMLIR_TABLEGEN=$NATIVE_BIN/mlir-tblgen
+        #-DMLIR_LINALG_ODS_YAML_GEN
+    )
+fi
+
+cmake -G Ninja "${CMAKE_HOST_ARGS[@]}" \
+    -Bllvm-project/build llvm-project/llvm
 cmake --build llvm-project/build -j "${MAX_JOBS}"
 
 export LLVM_SYSPATH=$PWD/llvm-project/build
